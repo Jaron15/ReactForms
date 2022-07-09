e96d1289e559e09711f8b01fd928519ca3cdbc11
Jaron <jjkenyon777@gmail.com>
2022-07-08 17:28:49 -0700

    second example first name setup

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,55 @@
+import {useState} from 'react';
+
 const BasicForm = (props) => {
+  const [enteredFirstName, setEnteredFirstName] = useState('');
+  const [firstNameTouched, setFirstNameTouched] = useState(false);
+  // const [enteredLastName, setEnteredLastName] = useState('');
+  // const [enteredEmail, setEnteredEmail] = useState('');
+
+  const firstNameIsValid = enteredFirstName !== '';
+  const firstNameIsNotValid = !firstNameIsValid && firstNameTouched;
+
+  const FNChangeHandler = (event) => {
+    setEnteredFirstName(event.target.value);
+  }
+
+  const FNBlurHandler = () => {
+    setFirstNameTouched(true)
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (firstNameIsNotValid) {
+      return 
+    }
+
+    console.log(enteredFirstName);
+    setEnteredFirstName('');
+    setFirstNameTouched(false);
+
+  }
+
+  let formIsValid = false;
+  if (firstNameIsValid) {
+    formIsValid = true
+  } 
+
+const FNInputClasses = firstNameIsNotValid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={FNInputClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input 
+          type='text' 
+          id='name' 
+          onChange={FNChangeHandler} 
+          onBlur={FNBlurHandler}
+          value={enteredFirstName}
+          />
+          {firstNameIsNotValid && <p>Please enter a first name</p>}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
@@ -16,7 +61,7 @@ const BasicForm = (props) => {
         <input type='text' id='name' />
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
---
+++ src/hooks/useInputs.js
@@ -0,0 +1,8 @@
+import {useState} from 'react';
+
+const useInputs = (validateValue) =>  {
+ const [enteredValue, setEnteredValue] = useState('');
+ const [isTouched, setIsTouched] = useState(false);
+
+ 
+}
